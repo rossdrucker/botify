1424c50782d830f4deee6a87d517336f14beedb8
Ross Drucker <ross.a.drucker@gmail.com>
2020-06-30 17:16:39 -0500

    Added auto-writing functionality for username, discover weekly playlist ID, and the desired playlist to move things to, as well as removing my own personal details from the original file.

--- move_discover_weekly_botify.py
+++ move_discover_weekly_botify.py
@@ -3,14 +3,36 @@
 @author: Ross Drucker
 """
 
-from botifunctions import authorize, get_track_ids_from_playlist
+from botifunctions import authorize, get_track_ids_from_playlist, add_tracks_to_playlist
 
-discover_weekly_id = '37i9dQZEVXcPgVqvfqWHGE' # Discover Weekly
+try:
+    from botify_settings import username
+except:
+    username = input('Spotify Username: ')
+    with open('botify_settings.py', 'a') as f:
+        f.write(f"username = '{username}'")
+        f.close()
 
-user_id = 'rossdrucker9'
+try:
+    from botify_settings import discover_weekly_id
+except:
+    discover_weekly_id = input('Discover Weekly playlist ID: ')
+    with open('botify_settings.py', 'a') as f:
+        f.write(f"discover_weekly_id = '{discover_weekly_id}'")
+        f.close()
 
+try:
+    from botify_settings import discover_weeklies_id
+except:
+    discover_weeklies_id = input('Discover Weeklies playlist ID: ')
+    with open('botify_settings.py', 'a') as f:
+        f.write(f"discover_weeklies_id = '{discover_weeklies_id}'")
+        f.close()
+        
 sp = authorize()
 
 discover_weekly_tracks = get_track_ids_from_playlist(sp, playlist_id = discover_weekly_id)
 
-sp.user_playlist_add_tracks(user_id, '17NCMIxHftu2lNiYav25t0', discover_weekly_tracks)
+add_tracks_to_playlist(sp, discover_weekly_tracks, discover_weeklies_id)
+
+print(f'Added {len(discover_weekly_tracks)} to Discover Weeklies')
